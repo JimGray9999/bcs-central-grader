a9386f1a332f6aa96efadc20ede9228524ecad1d
Jim Gray <jimgray2005@gmail.com>
2019-10-20 10:44:35 -0400

    added more api requests

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 require('dotenv').config();
 const axios = require('axios');
 const baseUrl = 'https://www.bootcampspot.com/api/instructor/v1';
-
+let authToken = '';
 
 axios.post(`${baseUrl}/login`, {
     email: process.env.USERNAME,
     password: process.env.PASSWORD
   })
   .then(function (response) {
-    console.log(response);
+    authToken = response.data.authenticationInfo.authToken;
+    getAboutMe();
   })
   .catch(function (error) {
     console.log(error);
   });
 
-
 function getAboutMe() {
-    axios.get({});
+    axios.get(`${baseUrl}/me`, {
+        headers: {
+            'Content-Type': 'application/json',
+            'authToken': authToken
+        }
+    })
+    .then(function (response) {
+        console.log(`Your name is: ${response.data.userAccount.firstName} ${response.data.userAccount.lastName}`);
+        console.log(`You have a total of ${response.data.enrollments.length} cohorts`);
+      })
+    .catch(function (error) {
+    console.log(error);
+    });
 }
