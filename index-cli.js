5ebc8a357f9b11d5dae39da12d2ae77ea6fbc98f
Jim Gray <jimgray2005@gmail.com>
2019-11-01 20:54:52 -0400

    fixed inquirer steps

--- index-cli.js
+++ index-cli.js
@@ -1,24 +1,15 @@
 require('dotenv').config();
 const axios = require('axios');
 const baseUrl = 'https://www.bootcampspot.com/api/instructor/v1';
-const fs = require('fs');
+const fse = require('fse');
 const inquirer = require('inquirer');
 let authToken = '';
 let courseID = '';
+let cohortInfo = [];
 
 function init() {
   login()
     .then(mainMenu());
-}
-
-  
-let cohortInfo;
-
-let cohortQuestion = {
-  type: 'rawlist',
-  name: 'menuChoice',
-  message: 'Select a cohort',
-  cohortInfo
 }
 
 function mainMenu() {
@@ -30,6 +21,10 @@ function mainMenu() {
     {
       name: 'Show ungraded assignments',
       value: 'ungraded'
+    },
+    {
+      name: 'Refresh data (run if first time)',
+      value: 'refresh'
     },
     {
       name: 'Exit BCS Central Grader',
@@ -48,18 +43,21 @@ function mainMenu() {
   .prompt([question])
   .then(({menuChoice}) => {
     switch (menuChoice) {
-      case 'cohorts':
+      case 'cohorts': 
         cohortsMenu();
         break;
       case 'ungraded':
         console.log("TODO: Show all ungraded assignments here.");
         mainMenu();
         break;
+      case 'refresh':
+        getCohortList();
+        mainMenu();
+        break;
       case 'exit':
         process.exit();
       default:
         console.log("...I'm not sure what you are looking for.");
-        console.log(`Auth token: ${authToken}`);
         mainMenu();
     }
   });
@@ -70,7 +68,7 @@ function mainMenu() {
 // function to write to a json the reponse data received
 function writeToFile (data, path) {  
   const json = JSON.stringify(data, null, 2)
-  fs.writeFile(path, json, (err) => {
+  fse.writeFile(path, json, (err) => {
     if (err) {
       console.error(err)
       throw err
@@ -79,7 +77,7 @@ function writeToFile (data, path) {
 }
 
 function readAFile (path) {
-  fs.readFileSync(path, (err, data) => {
+  fse.readFileSync(path, (err, data) => {
     if (err) throw err;
     console.log(data);
     return JSON.parse(data);
@@ -95,7 +93,6 @@ function login() {
     })
     .then(function (response) {
       authToken = response.data.authenticationInfo.authToken;
-      getCohortList();
       resolve();
     })
     .catch(function (error) {
@@ -106,7 +103,6 @@ function login() {
 }
 
 function getCohortList() {
-    let cohortArray = [];
     axios.get(`${baseUrl}/me`, {
       headers: {
           'Content-Type': 'application/json',
@@ -115,24 +111,42 @@ function getCohortList() {
     })
     .then(function (response) {
         for (let i = 0; i < response.data.enrollments.length; i++) {
-          cohortArray.push(
-              {'Cohort Name': response.data.enrollments[i].course.cohort.program.name,
-              'value': response.data.enrollments[i].course.cohortId
+          cohortInfo.push(
+              {'name': response.data.enrollments[i].course.cohort.program.name,
+              'value': response.data.enrollments[i].id
               }
           )
         }
-        writeToFile(cohortArray, './cohorts.json');
-        cohortInfo = readAFile('./cohorts.json');
+        writeToFile(cohortInfo, './cohorts.json');
       })
 }
 
 function cohortsMenu() {
-  console.log(cohortInfo);
-  // inquirer.prompt([cohortQuestion])
-  // .then(({menuChoice}) => {
-  //     console.log(`TODO: Do something with cohort choice ${menuChoice} here.`);
-  //     mainMenu();
-  // })
+  let cohortQuestion = {
+    type: 'rawlist',
+    name: 'menuChoice',
+    message: 'Select a cohort',
+    choices: cohortInfo
+  }
+  
+  inquirer.prompt([cohortQuestion])
+  .then(({menuChoice}) => {
+      assignments(menuChoice);
+      mainMenu();
+  })
+}
+
+function assignments(enrollmentId) {
+  axios.post(`${baseUrl}/assignments`, {
+    "enrollmentId": enrollmentId,
+    headers: {
+        'Content-Type': 'application/json',
+        'authToken': authToken
+    }
+  })
+  .then(function (response) {
+      console.log(response);
+    })
 }
 
 init();
